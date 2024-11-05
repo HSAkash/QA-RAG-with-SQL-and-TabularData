52d2aa60d225ba017d0902d37e4b4aaf9c5e0cf9
HSAkash <hemelakash472@gmail.com>
2024-11-05 12:52:33 +0600

    create project template

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "QA-RAG-with-SQL-and-TabularData"
+AUTHOR_USER_NAME = "HSAkash"
+SRC_REPO = "qaRagSqlTabularData"
+AUTHOR_EMAIL = "hemelakash472@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="""This project is a template for creating a QA system using RAG with SQL and Tabular Data""",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
